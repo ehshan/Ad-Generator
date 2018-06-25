4bf0122e6129b84b36e60383676106223d6724ae
Ehshan <ehshan@msn.com>
2018-06-25 21:12:05 +0100

    added functions to tokenize entire directory

--- clean_data.py
+++ clean_data.py
@@ -2,6 +2,8 @@ import os
 import glob
 import csv
 
+from nltk.tokenize import RegexpTokenizer, word_tokenize
+
 # max out file size limit
 csv.field_size_limit(999999999)
 
@@ -11,28 +13,36 @@ data_path = os.path.abspath(os.path.join(os.getcwd(), '../../Data/Twitter-Data/C
 extension = 'csv'
 
 
-def read_text(filename):
-    # text
-    text = []
+def pre_process(sentence):
+    sentence = sentence.lower()
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(sentence)
+    return word_tokenize(" ".join(tokens))
+
+
+def tokenize_text(filename):
+    # tokens is a list type
+    tokens = []
     # for each line, tokenize words
     for line in filename:
         for field in line:
             stripped = field.strip('\"')
             if not (stripped.startswith('@') or stripped.startswith('RT')):
-                text.append(field)
-    print(text)
-    return text
+                cleaned = pre_process(stripped)
+                tokens.append(cleaned)
+    print(tokens)
+    return tokens
 
 
 # Open all files in Direction with extension
 def open_dir(path, extension):
-    corpus = []
+    tokens = []
     os.chdir(path)
     files = [i for i in glob.glob('*.{}'.format(extension))]
     for file in files:
-        print("Opening {}.".format(file))
+        print("Tokenizing {}.".format(file))
         f = csv.reader(open(file, 'rU', encoding='latin-1'), delimiter="\n", quotechar='|')
-        corpus.append(read_text(f))
+        tokens += tokenize_text(f)
     return corpus
 
 
