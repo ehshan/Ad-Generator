086541ed8ad463b6820db8a5de79ef95eca81224
Ehshan <ehshan@msn.com>
2018-07-01 15:42:58 +0100

    added logging

--- primary.py
+++ primary.py
@@ -1,12 +1,25 @@
-from clean_data import tokenize_dir
 import os
+import time
 
+from clean_data import tokenize_dir
+
+# working directory
 path = os.getcwd()
 
 # define data file and file extension
 data_path = os.path.abspath(os.path.join(os.getcwd(), '../../Data/Twitter-Data/Clean'))
 extension = 'csv'
 
+# START
+# ----
+
+# LOAD DATA
+# --
+
+print('\nLoading data...')
+
+print('Start-Time: ', time.ctime(time.time()))
 corpus = tokenize_dir(data_path, extension)
+print('End-Time: ', time.ctime(time.time()))
 
 print(corpus)
