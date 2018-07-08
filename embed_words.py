fd12f0f1e61a4a71de60ca2e805cdb2d02841ae2
Ehshan <ehshan@msn.com>
2018-07-08 14:47:46 +0100

    added dictionary lookup function - pass a model and return the dictionary/reverse dictionary functions

--- embed_words.py
+++ embed_words.py
@@ -19,3 +19,42 @@ def train_word_model(sentences, name):
     print("\nSaving fastText model to..." + path + "/Embeddings/")
     word_model.save(path + "/Embeddings/" + name + ".model")
     return word_model
+
+
+# return dictionary lookup functions when passed an embedding model
+def dictionary_lookups(model):
+    # get model data
+    embed_weights = model.wv.syn0
+    vocab_size, embedding_size = embed_weights.shape
+
+    # DICTIONARY/REVERSE DICTIONARY LOOKUPS
+
+    # Get the index for word in vocab
+    def word_to_index(word):
+        return model.wv.vocab[word].index
+
+    # Get index for similar word if not in vocab
+    def similar_word_to_index(word):
+        # most similar word
+        dict_word = model.wv.most_similar(word)[0][0]
+        return model.wv.vocab[dict_word].index
+
+    # check if word  and apply correct index function
+    def word_index(word):
+        if word in model.wv.vocab:
+            return word_to_index(word)
+        else:
+            # if no ngrams present return <ukn>
+            try:
+                return similar_word_to_index(word)
+            except KeyError:
+                return vocab_size - 1
+
+    # lookup word for index with  coded special character for unknown
+    def index_to_word(index):
+        if index == vocab_size - 1:
+            return '<UNK>'
+        else:
+            return model.wv.index2word[index]
+
+    return word_index, index_to_word
