27840b73055b147f486271825316b8b69b2b5bdd
Ehshan <ehshan@msn.com>
2018-07-31 22:21:43 +0100

    added load and transform data function to conditioned script

--- conditioned.py
+++ conditioned.py
@@ -1,6 +1,27 @@
 import os.path
+import time
+
+from clean_data import tokenize_dir
 
 # self path
 path = os.getcwd()
+
+# define data file and file extension
+data_path = os.path.abspath(os.path.join(os.getcwd(), '../../Data/Twitter-Data/Clean'))
+extension = 'csv'
+
+
 # for saving
 version_name = 'conditioned_train'
+
+# START
+# ----
+
+# LOAD DATA
+# --
+
+print('\nLoading data...')
+
+print('Start-Time: ', time.ctime(time.time()))
+corpus = tokenize_dir(data_path, extension)
+print('End-Time: ', time.ctime(time.time()))
