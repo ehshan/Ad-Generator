5d27a092ceb772e4771d58ecd5eeac0fde3c7b93
Ehshan <ehshan@msn.com>
2018-08-07 17:50:16 +0100

    added function to create similar labels to a set of keywords using word model

--- label_corpus.py
+++ label_corpus.py
@@ -1,5 +1,3 @@
-
-
 # ADD START AND END TAGS TO TARGET DATA
 # -------------------------------------
 
@@ -12,3 +10,14 @@ def tag_corpus(sentences):
         sentence.append('<END>')
         all_sentences.append(sentence)
     return all_sentences
+
+
+tags = ['engine', 'body', 'speed', 'elegance', 'safety']
+
+
+# build a set of labels of each keyword
+def tag_builder(word, word_model):
+    tags = word_model.most_similar(word)
+    labels = [i[0] for i in tags]
+    labels.append(word)
+    return labels
