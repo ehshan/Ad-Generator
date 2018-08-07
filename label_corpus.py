d55f5d6c4ef5ee72b7defe821b1a0f8ed7cae50b
Ehshan <ehshan@msn.com>
2018-08-07 19:26:11 +0100

    added function to apply tag_builder function to a list of lists

--- label_corpus.py
+++ label_corpus.py
@@ -1,6 +1,7 @@
 # ADD START AND END TAGS TO TARGET DATA
 # -------------------------------------
 
+
 # add start and end tags to corpus
 def tag_corpus(sentences):
     print("\nTagging corpus...")
@@ -12,6 +13,9 @@ def tag_corpus(sentences):
     return all_sentences
 
 
+# LABEL CORPUS
+# ------------
+
 tags = ['engine', 'body', 'speed', 'elegance', 'safety']
 
 
@@ -21,3 +25,11 @@ def tag_builder(word, word_model):
     labels = [i[0] for i in tags]
     labels.append(word)
     return labels
+
+
+# return a list with lists of labels for each keyword
+def all_tags(list, word_model):
+    tags = []
+    for word in list:
+        tags.append(tag_builder(word, word_model))
+    return tags
